d67bb0f3eac1f20daceababf16d8f47102306721
Peter Aigner <mastapetz@icloud.com>
2023-07-14 17:43:54 +0200

    Anlegen, Abbrechen Props und methoden; konstruktor

--- CVAVT/ViewModels/NeueAktivitaetViewModel.cs
+++ CVAVT/ViewModels/NeueAktivitaetViewModel.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CVAVT.ViewModels
 {
     internal class NeueAktivitaetViewModel : WpfLibrary.BaseViewModel
     {
+        // AktiviaetAnlegen
+        public ICommand AktivitaetAnlegenCmd { get; set; }
+        public ICommand AnlegenAbbrechenCmd { get; set; }
+
+        // Konstruktor
+        public NeueAktivitaetViewModel()
+        {
+            AktivitaetAnlegenCmd = new WpfLibrary.RelayCommand(AktivitaetAnlegen);
+            AnlegenAbbrechenCmd = new WpfLibrary.RelayCommand(AnlegenAbbrechen);
+        }
+
+        private void AktivitaetAnlegen()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AnlegenAbbrechen()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
