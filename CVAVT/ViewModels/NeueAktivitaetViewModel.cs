c71232debaf535f8b3e76cc6747ff5af971fe4e2
Peter Aigner <mastapetz@icloud.com>
2023-07-14 17:55:59 +0200

    properties angelegt

--- CVAVT/ViewModels/NeueAktivitaetViewModel.cs
+++ CVAVT/ViewModels/NeueAktivitaetViewModel.cs
@@ -11,7 +11,20 @@ namespace CVAVT.ViewModels
     {
         // AktiviaetAnlegen
         public ICommand AktivitaetAnlegenCmd { get; set; }
+
+        // Anlegen Abbrechen
         public ICommand AnlegenAbbrechenCmd { get; set; }
+
+        // Aktivitaet  Properties
+        public string AktivitaetenName { get; set; }
+        public string AktivitaetenLeiter { get; set; }
+        public string AktivitaetenArt { get; set; }
+        public DateOnly AktivitaetenDatum { get; set; }
+        public TimeOnly AktivitaetenZeit { get; set; }
+        public double AktivitaetenDauer { get; set; }
+        public int AktivitaetenMaxTeilnehmer { get; set; }
+        public bool AktivitaetenVorwissenNoetig { get; set; }
+        public string AktivitaetenInformation { get; set; }
 
         // Konstruktor
         public NeueAktivitaetViewModel()
