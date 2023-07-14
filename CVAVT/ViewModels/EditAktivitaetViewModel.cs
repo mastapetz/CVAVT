18c9728ad947e8b79808b1b46640fd2730753a1f
Peter Aigner <mastapetz@icloud.com>
2023-07-14 23:51:29 +0200

    Edit ViewModel angelegt

---
+++ CVAVT/ViewModels/EditAktivitaetViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CVAVT.ViewModels
+{
+    internal class EditAktivitaetViewModel : WpfLibrary.BaseViewModel
+    {
+        // AktiviaetAnlegen
+        public ICommand AktivitaetEditCmd { get; set; }
+
+        // Anlegen Abbrechen
+        public ICommand EditAbbrechenCmd { get; set; }
+
+        // Aktivitaet  Properties
+        public string AktivitaetenName { get; set; }
+        public string AktivitaetenLeiter { get; set; }
+        public string AktivitaetenArt { get; set; }
+        public DateOnly AktivitaetenDatum { get; set; }
+        public TimeOnly AktivitaetenZeit { get; set; }
+        public double AktivitaetenDauer { get; set; }
+        public int AktivitaetenMaxTeilnehmer { get; set; }
+        public bool AktivitaetenVorwissenNoetig { get; set; }
+        public string AktivitaetenInformation { get; set; }
+
+        // Konstruktor
+        public EditAktivitaetViewModel()
+        {
+            AktivitaetEditCmd = new WpfLibrary.RelayCommand(AktivitaetEdit);
+            EditAbbrechenCmd = new WpfLibrary.RelayCommand(EditAbbrechen);
+        }
+
+        private void AktivitaetEdit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EditAbbrechen()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
