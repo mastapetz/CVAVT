1c1180172a4f7b59d23bab4605ab51b8ed795fc1
Peter Aigner <mastapetz@icloud.com>
2023-07-19 00:56:59 +0200

    VIewModel für Leiter erstellt

---
+++ CVAVT/ViewModels/NeuerLeiterViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CVAVT.ViewModels
+{
+    internal class NeuerLeiterViewModel : WpfLibrary.BaseViewModel
+    {
+        // Commands
+        public ICommand EingabeVerwerfenCmd { get; set; }
+        public ICommand EingabeSpeichernCmd { get; set; }
+
+        // Properties
+        public string LeiterName { get; set; }
+
+        // Konstruktor
+        public NeuerLeiterViewModel()
+        {
+            EingabeVerwerfenCmd = new WpfLibrary.RelayCommand(EingabeVerwerfen);
+            EingabeSpeichernCmd = new WpfLibrary.RelayCommand(EingabeSpeichern);
+        }
+
+        private void EingabeVerwerfen()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        private void EingabeSpeichern()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
