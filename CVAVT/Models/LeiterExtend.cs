31e7631c291606d969732f54c2945d6e9f36f434
Peter Aigner <mastapetz@icloud.com>
2023-07-26 23:02:53 +0200

    Um funktion von combobox zu ermöglichen

---
+++ CVAVT/Models/LeiterExtend.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVAVT.Models
+{
+    public partial class Leiter
+    {
+        public override bool Equals(object obj)
+        {
+            if (obj is Leiter)
+            {
+                Leiter leiterCombo = obj as Leiter;
+                return this.LeiterId == leiterCombo.LeiterId;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return this.LeiterName.ToString();
+        }
+    }
+
+}
