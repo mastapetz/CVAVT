f47fe1445367885497ef30488211514e3c1c55fb
Peter Aigner <mastapetz@icloud.com>
2023-07-31 16:19:22 +0200

    Fenster Schließen FUnktion

--- CVAVT/ViewModels/NeuerLeiterViewModel.cs
+++ CVAVT/ViewModels/NeuerLeiterViewModel.cs
@@ -16,7 +16,7 @@ namespace CVAVT.ViewModels
         public ICommand EingabeSpeichernCmd { get; set; }
 
         // Event zum Schließen
-        public event EventHandler OnRequestCloseWindow;
+        public event EventHandler OnRequestCloseWin;
         // Properties
         //public string LeiterName { get; set; }
         public ObservableCollection<Leiter> LeiterListe { get; set; }
@@ -24,14 +24,24 @@ namespace CVAVT.ViewModels
         // Konstruktor
         public NeuerLeiterViewModel()
         {
-
+            LeiterListe = new ObservableCollection<Leiter>();
             EingabeVerwerfenCmd = new WpfLibrary.RelayCommand(EingabeVerwerfen);
             EingabeSpeichernCmd = new WpfLibrary.RelayCommand(EingabeSpeichern);
         }
+        /// <summary>
+        /// Funktion Verlassen schließt das Fenster
+        /// </summary>
+        private void Verlassen()
+        {
+            // Window schließen
+            if (OnRequestCloseWin != null)
+                // Delegate wird aufgerufen
+                OnRequestCloseWin(this, new EventArgs());
 
+        }
         private void EingabeVerwerfen()
         {
-            throw new NotImplementedException();
+            Verlassen();
         }
 
 
