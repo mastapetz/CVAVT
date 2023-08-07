3c409a5986488096c4e6dd6169a76cf9e955e4f9
Peter Aigner <mastapetz@icloud.com>
2023-08-07 13:06:28 +0200

    konvertierer zur formats anpassung

---
+++ CVAVT/Konverter/DateFormatConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CVAVT.Konverter
+{
+    internal class DateFormatConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                // Gibt Datum aus im Format (Beispiel): 01.01.2023; Tag.Monat.Jahr
+                // CultureInfo.InvariantCulture stellt sicher das Kulturelle Unterschiede bei der Formatierung keine ROlle Spielen
+                return dateTime.ToString("dd.MM.yyyy", CultureInfo.InvariantCulture);
+            }
+            return string.Empty;
+        }
+
+        // Wird benötigt für  " : IValueConverter "
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string dateString)
+            {
+                if (DateTime.TryParseExact(dateString, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateTime))
+                {
+                    return dateTime;
+                }
+            }
+            return DependencyProperty.UnsetValue; // oder eine andere Rückgabewert, wenn die Konvertierung fehlschlägt
+        }
+
+    }
+}
---
+++ CVAVT/Konverter/TimeFormatConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CVAVT.Konverter
+{
+    internal class TimeFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime.ToString("HH:mm");
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string timeString)
+            {
+                if (DateTime.TryParseExact(timeString, "HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime time))
+                {
+                    return time;
+                }
+            }
+            return DependencyProperty.UnsetValue; // oder eine andere Rückgabewert, wenn die Konvertierung fehlschlägt
+        }
+    }
+}
