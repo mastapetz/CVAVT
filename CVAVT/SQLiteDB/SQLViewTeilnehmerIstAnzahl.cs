742dca05415c1b4eac734a569b9ceb1f16d31243
Peter Aigner <mastapetz@icloud.com>
2023-08-08 20:42:25 +0200

    "View" für zählen der Ist teilnehmer (ungetestet)

---
+++ CVAVT/SQLiteDB/SQLViewTeilnehmerIstAnzahl.cs
@@ -0,0 +1,13 @@
+﻿// 
+//
+using System;
+using System.Collections.Generic;
+
+namespace CVAVT.SQLiteDB;
+
+public partial class SQLViewTeilnehmerIstAnzahl
+{
+    public int AktivitaetIdfk { get; set; }
+
+    public int? TeilnehmerIstAnzahl { get; set; }
+}
