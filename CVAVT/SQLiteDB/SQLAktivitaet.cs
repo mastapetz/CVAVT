f44a7b68032812d47afd3bcdbafae0d6c755c2ae
Peter Aigner <mastapetz@icloud.com>
2023-08-08 20:42:54 +0200

    angleichung an AKtivitaet.cs

--- CVAVT/SQLiteDB/SQLAktivitaet.cs
+++ CVAVT/SQLiteDB/SQLAktivitaet.cs
@@ -1,5 +1,6 @@
 ﻿// Auto generiert (Command siehe mainwindowviewmode in kommentaren) und umbennant
 // --
+using CVAVT.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,27 +9,47 @@ namespace CVAVT.SQLiteDB;
 
 public partial class SQLAktivitaet
 {
-    public long AktivitaetenId { get; set; }
+    public int AktivitaetenId { get; set; }
 
-    public string AktivitaetenName { get; set; } = null!;
+    public string AktivitaetenName { get; set; }
 
-    public string AktivitaetenArt { get; set; } = null!;
+    public string AktivitaetenArt { get; set; }
 
-    public byte[] AktivitaetenDatum { get; set; } = null!;
+    public DateTime AktivitaetenDatum { get; set; }
 
-    public byte[] AktivitaetenZeit { get; set; } = null!;
+    public DateTime AktivitaetenZeit { get; set; }
 
     public double AktivitaetenDauer { get; set; }
 
-    public long? AktivitaetenMaxTeilnehmer { get; set; }
+    public int? AktivitaetenMaxTeilnehmer { get; set; }
 
-    public long AktivitaetenVorwissenNoetig { get; set; }
+    public int AktivitaetenVorwissenNoetig { get; set; }
 
     public string? AktivitaetenInformation { get; set; }
 
-    public long LeiterIdfk { get; set; }
+    public int LeiterIdfk { get; set; }
 
-    public virtual SQLLeiter LeiterIdfkNavigation { get; set; } = null!;
+    public virtual SQLLeiter LeiterIdfkNavigation { get; set; }
 
     public virtual ICollection<SQLKategorie> KategorieIdfks { get; set; } = new List<SQLKategorie>();
+
+    // Um Ist auch im Mainwindow anzuzeigen
+    public int AktivitaetenIstTeilnehmer
+    {
+        get
+        {
+            using (SQLiteKontext context = new SQLiteKontext())
+            {
+                var viewTeilnehmer = context.ViewTeilnehmerIstAnzahl
+                    .FirstOrDefault(v => v.AktivitaetIdfk == this.AktivitaetenId);
+
+                if (viewTeilnehmer != null)
+                {
+                    return viewTeilnehmer.TeilnehmerIstAnzahl ?? 0;
+                }
+
+                return 0;
+            }
+        }
+    }
 }
