abfea0998394afbb67d44be07d11093d8ef21fb6
Peter Aigner <mastapetz@icloud.com>
2023-08-09 19:33:28 +0200

    Factories, derzeit inaktiv

---
+++ CVAVT/Factories/DbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using CVAVT.Models;
+using CVAVT.SQLiteDB;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVAVT.Factories
+{
+    internal class DbContextFactory
+    {
+        public static DbContext Create(bool useMSSQLVerbindung)
+        {
+            if (useMSSQLVerbindung)
+            {
+                var options = new DbContextOptions<CVAVTContext>();
+                return new CVAVTContext(options);
+            }
+            else
+            {
+                var options = new DbContextOptions<SQLiteKontext>();
+                return new SQLiteKontext(options);
+            }
+        }
+    }
+}
