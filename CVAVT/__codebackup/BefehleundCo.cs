4c940e2caefda0e9437532f9ea6251f8954a6f4a
Peter Aigner <mastapetz@icloud.com>
2023-08-12 01:25:19 +0200

    wpflib hinzugefügt

---
+++ CVAVT/__codebackup/BefehleundCo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVAVT.__codebackup
+{
+    internal class BefehleundCo
+    {
+        /*
+ * Befehl zum automatischen erstellen der Models für SQLite
+ * dotnet ef dbcontext scaffold "Data Source=E:\Programmieren lernen\c#\Wifi\ConventionVereinsAktivitaetenVerwaltungsTool\CVAVT\SQLiteDB\SQLite.db" Microsoft.EntityFrameworkCore.Sqlite -o "E:\Programmieren lernen\c#\Wifi\ConventionVereinsAktivitaetenVerwaltungsTool\CVAVT\SQLiteDB" -c SQLiteContext -f -n CVAVT.SQLiteDB
+ * 
+ * version 2
+ * dotnet ef dbcontext scaffold "Data Source=E:\Programmieren lernen\c#\Wifi\ConventionVereinsAktivitaetenVerwaltungsTool\CVAVT\SQLiteDB\SQLite.db" Microsoft.EntityFrameworkCore.Sqlite -o "E:\Programmieren lernen\c#\Wifi\ConventionVereinsAktivitaetenVerwaltungsTool\CVAVT\SQLiteDB" -c SQLiteKontext -f -n CVAVT.SQLiteDB
+ */
+    }
+}
---
+++ CVAVT/__codebackup/SettingundCoinMWVM.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVAVT.__codebackup
+{
+    internal class SettingundCoinMWVM
+    {
+
+        /*
+public bool UseMSSQLSMVerbindung
+{
+    get { return Properties.Settings.Default.UseMSSQLSMVerbindung; }
+    set
+    {
+        if (Properties.Settings.Default.UseMSSQLSMVerbindung != value)
+        {
+            Properties.Settings.Default.UseMSSQLSMVerbindung = value;
+            Properties.Settings.Default.Save();
+            OnPropertyChanged(nameof(UseMSSQLSMVerbindung));
+        }
+    }
+}
+
+public bool UseSQLiteVerbindung
+{
+    get { return Properties.Settings.Default.UseSQLiteVerbindung; }
+    set
+    {
+        if (Properties.Settings.Default.UseSQLiteVerbindung != value)
+        {
+            Properties.Settings.Default.UseSQLiteVerbindung = value;
+            Properties.Settings.Default.Save();
+            OnPropertyChanged(nameof(UseSQLiteVerbindung));
+        }
+    }
+}
+*/
+    }
+}
